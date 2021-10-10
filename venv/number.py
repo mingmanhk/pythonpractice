0eceda0486e01d767248457d392810e22060104e
Victor Lam <mingmanhk@gmail.com>
2021-10-10 00:43:24 -0700

    number

---
+++ venv/number.py
@@ -0,0 +1,36 @@
+from math import *
+
+print(2)
+
+print(2+0.99)
+
+
+print(2+0.99*(2*3))
+
+# remind after divide
+print(10 % 3)
+
+# convert number to string
+num=5
+print(str(num))
+
+#absult value
+print(abs(-5))
+
+#pow and squre
+print(pow(5,2))
+print(sqrt(36))
+
+#max and min
+array=[1, 3, 4, 5, 99]
+print(max(array))
+print(min(array))
+
+#round
+print(round(0.123456, 2))
+
+#floor and ceiling
+print(floor(312.723456))
+print(ceil(231.823456))
+
+
---
+++ venv/variable.py
@@ -0,0 +1,20 @@
+# Variable
+test_string= "Victor"
+test_integer = 30
+test_decmial = 0.12345
+test_boolean= True
+
+print("String " + test_string)
+print("Ineteger " + str(test_integer))
+print("Decimal " + str(test_decmial))
+print("Boolean " +  str(test_boolean))
+
+# String Position
+phrase ="String Test"
+print(phrase[0])
+
+# String letter position index
+print(phrase.index("T"))
+
+# String replace
+print(phrase.replace("Test", "Final"))
