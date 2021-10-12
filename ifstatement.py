7fed2bd3c58a30e69e6af12242b6d2acb269a370
mingmanhk@gmail.com <Victor0105!>
2021-10-12 00:04:33 -0700

    done if statement

--- ifstatement.py
+++ ifstatement.py
@@ -1,7 +1,9 @@
 is_male = True
-is_tall = True
+is_tall = False
 
 if is_male or is_tall:
     print ("tall and male")
+elif is_male and not is_tall:
+    print("male but not tall")
 else:
     print("You neither tall nor male")
