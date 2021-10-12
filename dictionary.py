12b0030f5a8341d172ba15c9425f70c2bb474507
mingmanhk@gmail.com <Victor0105!>
2021-10-12 00:37:27 -0700

    done dictionary statement

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+monthConversion = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+result = monthConversion.get("May")
+print(result)
+
+
