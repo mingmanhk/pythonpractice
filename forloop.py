5d9553b0ee627f867a65bb11123c4cfacc866ba3
mingmanhk@gmail.com <Victor0105!>
2021-10-13 23:23:46 -0700

    done for loop

---
+++ forloop.py
@@ -0,0 +1,12 @@
+for letter in "Victor":
+    print(letter)
+
+friends =["Jim", "Karen", "Kevin"]
+for name in friends:
+    print(name)
+
+for index in range(3,10):
+    print(index )
+
+for index in range(len(friends)):
+    print(friends[index])
