40ffa7f57702c663d583cf8a4b3824335c98ad8b
mingmanhk@gmail.com <Victor0105!>
2022-01-26 01:13:50 -0800

    add yourlifyinweeks

---
+++ yourlifeinweeks.py
@@ -0,0 +1,6 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+print('You have '+ str((90-int(age))*365) + ' days, '+str((90-int(age))*52)+' weeks, and '+str((90-int(age))*12)+' months left.')
