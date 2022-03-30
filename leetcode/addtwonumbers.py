80d1c8edabdfae22ac91993453107df340572f2f
Victor Lam <mingmanhk@gmail.com>
2022-03-29 23:15:48 -0700

    “Master”

---
+++ leetcode/addtwonumbers.py
@@ -0,0 +1,6 @@
+def addTwoNumbers(l1, l2):
+    res=0
+    ls
+    return res
+
+print(addTwoNumbers([2,4,3],[5,6,4]))
--- leetcode/playground.py
+++ leetcode/playground.py
@@ -1,7 +1,4 @@
-from tkinter import X
-
-
-def romanToInt(s):
+"""def romanToInt(s):
     res= 0  
     #decode= { "I": 0, "IV": 4, "V": 5, "IC": 9,"X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400,"D": 500, "CM": 900, "M": 1000}
     decode = {"I": 1, "V" : 5, "X" :10, "L": 50, "C": 100, "D" : 500, "M" : 1000}
@@ -14,3 +11,35 @@ def romanToInt(s):
     return res
 
 print(romanToInt("MCMXCIV")) 
+"""
+from audioop import reverse
+
+
+def intToRoman(nums):
+    res =[]
+    decode=  [["I", 1],
+    ["IV", 4],
+    ["V", 5],
+    ["IX", 9],
+    ["X", 10],
+    ["XL", 40],
+    ["L", 50],
+    ["XC", 90],
+    ["C", 100],
+    ["CD", 400],
+    ["D", 500],
+    ["CM", 900],
+    ["M", 1000]]
+
+    for i, v in reversed(decode):
+        print("---------------"+ i)
+        print("nums % a " + str(nums % v))
+        print("nums// v " + str(nums// v))
+        print("---------------")
+        if nums// v:
+            res +=(i* (nums //v))
+            print("res +=(i* (nums //v)) " + str((i* (nums //v))))
+            nums=nums % v
+    return ''.join(res)
+
+print(intToRoman(333))
