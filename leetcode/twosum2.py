c7f441aed85088c8ceb538e7be645e22ae67cc50
mingmanhk@gmail.com <Victor0105!>
2022-04-18 22:09:52 -0700

    Master

--- leetcode/twosum2.py
+++ leetcode/twosum2.py
@@ -2,10 +2,17 @@ nums =[3,2,4]
 target = 6
 
 def twosum( num, target):
-    for x in range(len(nums)):
-        for y in range(x+1, len(nums)):
-            if nums[x] + nums[y] == target:
-                return([x, y])
+    #for x in range(len(nums)):
+    #   for y in range(x+1, len(nums)):
+    #        if nums[x] + nums[y] == target:
+    #            return([x, y])
 
-answer = twosum(nums, target)
-print(answer)
+    #answer = twosum(nums, target)
+    hashmap = {}
+    for i, v in enumerate(nums):
+        diff = target -v
+        if diff in hashmap:
+            return [hashmap[diff] , i]
+        hashmap[v] = i
+
+print(twosum([3,2,4], target))
